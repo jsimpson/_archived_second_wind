6d0a760c7cad02ccc4a36077f530aca936fa6711
Jonathan Simpson <jsimpson.github@gmail.com>
2015-09-17 19:29:46 -0700

    fix typo in app settings migration

--- db/migrate/20150730031300_create_application_settings.rb
+++ db/migrate/20150730031300_create_application_settings.rb
@@ -1,7 +1,7 @@
 class CreateApplicationSettings < ActiveRecord::Migration
   def change
     create_table :application_settings do |t|
-      t.boolean :imperial, null: false, defaukt: false
+      t.boolean :imperial, null: false, default: false
 
       t.timestamps null: false
     end
