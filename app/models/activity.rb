5e1f61b76052e8c9fc17b3088a45795262a2389c
Jonathan Simpson <jsimpson.github@gmail.com>
2015-11-30 17:11:25 -0800

    better styling for class methods

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,49 +2,48 @@ class Activity < ActiveRecord::Base
   has_many :geo_points
   has_many :laps
 
-  has_attached_file :geo_route,
-    url: '/public/geo_routes/:id/:filename', path: ':rails_root:url'
-  validates_attachment_content_type :geo_route,
-    content_type: ['application/xml', 'application/octet-stream']
+  has_attached_file :geo_route, url: '/public/geo_routes/:id/:filename', path: ':rails_root:url'
+  validates_attachment_content_type :geo_route, content_type: ['application/xml', 'application/octet-stream']
   validates_attachment_file_name :geo_route, matches: [/gpx\Z/, /tcx\Z/]
 
   after_save :update_route, if: ->(obj) { obj.geo_route_processed == false && obj.geo_route.present? }
-
   scope :started_at, -> { order('started_at DESC') }
 
-  def self.group_mileage_by_month
-    unscoped
-      .group_by_month(:started_at, format: '%B %Y')
-      .sum(:total_distance)
-      .map { |m| { m[0] => (m[1] * 0.000621371).round(2) }.flatten }
-  end
+  class << self
+    def group_mileage_by_month
+      unscoped
+        .group_by_month(:started_at, format: '%B %Y')
+        .sum(:total_distance)
+        .map { |m| { m[0] => (m[1] * 0.000621371).round(2) }.flatten }
+    end
 
-  def self.find_by_period(period = Time.now.all_year)
-    where(started_at: period)
-  end
+    def find_by_period(period = Time.now.all_year)
+      where(started_at: period)
+    end
 
-  def self.sum_distance
-    sum(:total_distance) * 0.000621371
-  end
+    def sum_distance
+      sum(:total_distance) * 0.000621371
+    end
 
-  def self.sum_elevation_gain
-    sum(:total_elevation_gain) * 3.28084
-  end
+    def sum_elevation_gain
+      sum(:total_elevation_gain) * 3.28084
+    end
 
-  def self.sum_time
-    time = sum(:total_time)
-    seconds = time % 60
-    minutes = (time / 60) % 60
-    hours = time / 3600
-    "#{hours} hrs, #{minutes} mins, #{seconds} secs"
-  end
+    def sum_time
+      time = sum(:total_time)
+      seconds = time % 60
+      minutes = (time / 60) % 60
+      hours = time / 3600
+      "#{hours} hrs, #{minutes} mins, #{seconds} secs"
+    end
 
-  def self.average_heart_rate
-    average(:average_heart_rate)
-  end
+    def average_heart_rate
+      average(:average_heart_rate)
+    end
 
-  def self.sum_calories
-    sum(:total_calories)
+    def sum_calories
+      sum(:total_calories)
+    end
   end
 
   private
--- app/models/geo_point.rb
+++ app/models/geo_point.rb
@@ -1,42 +1,44 @@
 class GeoPoint < ActiveRecord::Base
   belongs_to :activity
 
-  def self.lat_lng
-    order('time ASC')
-      .pluck(:lat, :lng)
-      .reject { |point| point.any?(&:blank?) }
-      .map { |point| { lat: point[0].to_f, lng: point[1].to_f } }
-      .uniq
-  end
+  class << self
+    def lat_lng
+      order('time ASC')
+        .pluck(:lat, :lng)
+        .reject { |point| point.any?(&:blank?) }
+        .map { |point| { lat: point[0].to_f, lng: point[1].to_f } }
+        .uniq
+    end
 
-  def self.heart_rate
-    pluck(:time, :heart_rate)
-      .reject { |point| point.any?(&:blank?) }
-      .map { |point| { point[0] => point[1] }.flatten }
-      .uniq
-  end
+    def heart_rate
+      pluck(:time, :heart_rate)
+        .reject { |point| point.any?(&:blank?) }
+        .map { |point| { point[0] => point[1] }.flatten }
+        .uniq
+    end
 
-  def self.heart_rate_intensity
-    points = pluck(:heart_rate).reject(&:blank?)
-    count = points.count
+    def heart_rate_intensity
+      points = pluck(:heart_rate).reject(&:blank?)
+      count = points.count
 
-    points
-      .group_by { |x| x / 10 }
-      .sort
-      .map { |k, vs| { ((10 * k)..(10 * k + 10)) => ((vs.count.to_f / count.to_f) * 100.0).round(2) }.flatten }
-  end
+      points
+        .group_by { |x| x / 10 }
+        .sort
+        .map { |k, vs| { ((10 * k)..(10 * k + 10)) => ((vs.count.to_f / count.to_f) * 100.0).round(2) }.flatten }
+    end
 
-  def self.elevation
-    pluck(:time, :elevation)
-      .reject { |point| point.any?(&:blank?) }
-      .map { |point| { point[0] => (point[1] * 3.28084).round(2) }.flatten }
-      .uniq
-  end
+    def elevation
+      pluck(:time, :elevation)
+        .reject { |point| point.any?(&:blank?) }
+        .map { |point| { point[0] => (point[1] * 3.28084).round(2) }.flatten }
+        .uniq
+    end
 
-  def self.speed
-    pluck(:time, :speed)
-      .reject { |point| point.any?(&:blank?) }
-      .map { |point| { point[0] => (point[1] * 2.23694).round(2) }.flatten }
-      .uniq
+    def speed
+      pluck(:time, :speed)
+        .reject { |point| point.any?(&:blank?) }
+        .map { |point| { point[0] => (point[1] * 2.23694).round(2) }.flatten }
+        .uniq
+    end
   end
 end
